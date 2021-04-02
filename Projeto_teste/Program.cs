c03f1e3fc52acebbdf0cfabeb4188d757b1392cf
Davisson Assuncao <davisson.assuncao@gmail.com>
2021-04-02 17:43:21 -0300

    Acrescentado a mensagem de bom dia

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
