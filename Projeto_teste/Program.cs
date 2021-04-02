b59b7e123a1bdc72d32443cafa05788f63c92712
Davisson Assuncao <davisson.assuncao@gmail.com>
2021-04-02 17:36:22 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Projeto_teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
