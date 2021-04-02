b1a0129157e620aa8a9c4b135a8dafd12d397cfc
Davisson Assuncao <davisson.assuncao@gmail.com>
2021-04-02 18:43:09 -0300

    Adicionado a mensagem 'como você está hoje'?

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje? ");
         }
     }
 }
