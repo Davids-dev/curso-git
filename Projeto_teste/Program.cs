4211b0b7c3c8210f29b713f95a1b559e6250dc97
Davisson Assuncao <davisson.assuncao@gmail.com>
2021-04-03 16:57:03 -0300

    Acrescentando a mensagem 'Desejando um bom dia'

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -7,8 +7,9 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje? ");
+            Console.WriteLine("Tenha um bom dia"s);
+
         }
     }
 }
